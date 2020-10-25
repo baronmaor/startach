2726df52fee9e004c49cac54c165a6fb017cdf7b
maor bar-on <maor@sonarax.com>
2020-10-25 10:17:24 +0200

    firstTemplaeBuild

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { BookingFlightsComponent } from './booking-flights/booking-flights.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookingFlightsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/booking-flights/booking-flights.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-flights',
+  templateUrl: './booking-flights.component.html',
+  styleUrls: ['./booking-flights.component.scss']
+})
+export class BookingFlightsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
